36f5e605b19dd572bfd339d3c83a75cedc002e18
Zhe Xu <zhexu@Zhes-MacBook-Pro.local>
2020-05-12 23:31:14 -0400

    try Jenkinsfile 7

---
+++ jenkins/build/bundle.sh
@@ -0,0 +1,9 @@
+echo "****************************"
+echo "********Building Webpack********"
+echo "****************************"
+
+WORKSPACE=/Users/zhexu/Desktop/CS/Github/posthem-react
+
+cd $WORKSPACE
+
+npm run build
