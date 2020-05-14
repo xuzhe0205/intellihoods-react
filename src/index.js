2367473e52a6ccd4321f9caf462e232735f0d9af
Zhe Xu <zhexu@Zhes-MacBook-Pro.local>
2020-05-14 15:38:55 -0400

    CICD succeeded, change website title2

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./store/reducer";
 import { BrowserRouter } from "react-router-dom";
-// import registerServiceWorker from "./registerServiceWorker";
+// import registerServiceWorker from "./registerdsddasdsaServiceWorker";
 
 const store = createStore(reducer);
 
