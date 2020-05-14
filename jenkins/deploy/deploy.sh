67faada5672f2ca6972f1e1dd236d253f573665b
Zhe Xu <zhexu@Zhes-MacBook-Pro.local>
2020-05-14 14:59:32 -0400

    test cicd, change permission6

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,9 +4,6 @@ echo posthem-react-nginx > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo Qiantu2013 >> /tmp/.auth
 
-echo "what"
 scp -i /opt/prod /tmp/.auth prod-user@ec2-54-82-166-255.compute-1.amazonaws.com:/tmp/.auth
-echo "what????"
 scp -i /opt/prod ./jenkins/deploy/publish.sh prod-user@ec2-54-82-166-255.compute-1.amazonaws.com:/tmp/publish.sh
-echo "what!!!!"
-ssh -i /opt/prod prod_user@ec2-54-82-166-255.compute-1.amazonaws.com "/tmp/publish.sh"
+ssh -i /opt/prod prod-user@ec2-54-82-166-255.compute-1.amazonaws.com "/tmp/publish.sh"
