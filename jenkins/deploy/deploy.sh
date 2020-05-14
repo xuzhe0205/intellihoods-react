f5c07008aefcb42058348d51b0b72ebacf585a14
Zhe Xu <zhexu@Zhes-MacBook-Pro.local>
2020-05-14 15:14:45 -0400

    test cicd, change permission8

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -8,4 +8,3 @@ echo Qiantu2013 >> /tmp/.auth
 scp -i /opt/prod /tmp/.auth prod-user@ec2-54-82-166-255.compute-1.amazonaws.com:/tmp/.auth
 scp -i /opt/prod ./jenkins/deploy/publish.sh prod-user@ec2-54-82-166-255.compute-1.amazonaws.com:/tmp/publish.sh
 ssh -i /opt/prod prod-user@ec2-54-82-166-255.compute-1.amazonaws.com "/tmp/publish.sh"
-echo "what"
