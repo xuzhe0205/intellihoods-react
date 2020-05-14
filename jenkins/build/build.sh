e4b6e411ec683ce60932c094e870c2aa458f7617
Zhe Xu <zhexu@Zhes-MacBook-Pro.local>
2020-05-14 17:21:55 -0400

    CICD succeeded, change website title11

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+echo $(ls jenkins/build/docker-image/dist)
+
 # Copy the dist/ to the build location
 cp -R dist jenkins/build/docker-image/dist
+
+cat jenkins/build/docker-image/dist/index.html
 
 echo "****************************"
 echo "** Building Docker Image in Jenkins Image***"
