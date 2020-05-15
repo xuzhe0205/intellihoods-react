fabcbd359cae850de809fa8db0fe4af4ed07af83
Zhe Xu <zhexu@Zhes-MacBook-Pro.local>
2020-05-14 23:19:57 -0400

    try2

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -5,6 +5,6 @@ echo $BUILD_TAG >> /tmp/.auth
 echo Qiantu2013 >> /tmp/.auth
 
 
-scp -i /opt/prod /tmp/.auth prod-user@ec2-54-234-200-183.compute-1.amazonaws.com:/tmp/.auth
-scp -i /opt/prod ./jenkins/deploy/publish.sh prod-user@ec2-54-234-200-183.compute-1.amazonaws.com:/tmp/publish.sh
-ssh -i /opt/prod prod-user@ec2-54-234-200-183.compute-1.amazonaws.com "/tmp/publish.sh"
+scp -v /opt/prod /tmp/.auth prod-user@ec2-54-234-200-183.compute-1.amazonaws.com:/tmp/.auth
+scp -v /opt/prod ./jenkins/deploy/publish.sh prod-user@ec2-54-234-200-183.compute-1.amazonaws.com:/tmp/publish.sh
+ssh -v /opt/prod prod-user@ec2-54-234-200-183.compute-1.amazonaws.com "/tmp/publish.sh"
