4470ed815ab8a951b9fcb02b0e3598699b59a2cb
Zhe Xu <zhexu@Zhes-MacBook-Pro.local>
2020-05-27 14:18:11 -0400

    more changes to uploading avatar in redirect profile page

--- src/component/RedirectProfileCard/RedirectProfileCard.js
+++ src/component/RedirectProfileCard/RedirectProfileCard.js
@@ -4,14 +4,19 @@ import Card from "../../component/Card/Card";
 import CardHeader from "../../component/Card/CardHeader";
 import CardBody from "../../component/Card/CardBody";
 import CustomInput from "../../component/CustomInput/CustomInput";
+import { Button, Alert } from "@material-ui/core";
 
 class RedirectProfileCard extends Component {
   constructor(props) {
     super(props);
     this.state = {
       bio: "",
+      file: "",
       avatar: null,
+      error: "",
+      msg: "",
     };
+    this.onFileChange = this.onFileChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
   }
@@ -26,9 +31,30 @@ class RedirectProfileCard extends Component {
     });
   }
 
+  onFileChange = (event) => {
+    this.setState({
+      file: event.target.files[0],
+    });
+  };
+
   handleSubmit(event) {
     event.preventDefault();
     const updateProfileRequest = Object.assign({}, this.state);
+    this.setState({ error: "", msg: "" });
+
+    if (!this.state.file) {
+      this.setState({ error: "Please upload a file." });
+      return;
+    }
+
+    if (this.state.file.size >= 2000000) {
+      this.setState({ error: "File size exceeds limit of 2MB." });
+      return;
+    }
+    alert(this.state.file.size);
+    console.log("check redirect");
+    console.log(this.state.file);
+    console.log(this.state);
   }
   render() {
     return (
@@ -47,7 +73,7 @@ class RedirectProfileCard extends Component {
           <br />
           <br />
           <br />
-          <span className="redirectProfileTitle">Update Profile</span>
+          <span className="redirectProfileTitle">Add profile bio</span>
 
           <CustomInput
             labelText="Your bio here..."
@@ -67,16 +93,29 @@ class RedirectProfileCard extends Component {
           <br />
           <div className="redirectProfileTitle">Upload a profile picture</div>
           <div id="redirectUploadProfileContainer">
-            <img
-              src={require("../../asset/image/addAvatar.png")}
-              id="redirectAvatarBtn"
-              alt="Add profile avatar"
-            />
+            <div className="image-upload">
+              <label htmlFor="file-input">
+                <img
+                  src={require("../../asset/image/addAvatar.png")}
+                  id="redirectAvatarBtn"
+                  alt="Add profile avatar"
+                />
+              </label>
+              <input
+                id="file-input"
+                onChange={this.onFileChange}
+                type="file"
+                accept="image/*"
+              ></input>
+            </div>
+
             <h4 style={{ color: "red" }}>{this.state.error}</h4>
             <h4 style={{ color: "green" }}>{this.state.msg}</h4>
             <br />
-            <input onChange={this.onFileChange} type="file"></input>
-            <button onClick={this.uploadFile}>Upload</button>
+
+            <Button variant="contained" type="submit">
+              Upload
+            </Button>
           </div>
         </CardBody>
       </form>
